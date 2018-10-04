eb314fe9f736d6d8b2473e109d58456851b0dce2
Katrina <pritckat@gmail.com>
2018-10-04 16:01:45 -0500

    no error for logging in with incorrect username

--- library-app/app/controllers/sessions_controller.rb
+++ library-app/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     def create
         #raise params
         @user = User.find_by(username: params[:user][:username])
-        if @user.authenticate(params[:user][:password])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
