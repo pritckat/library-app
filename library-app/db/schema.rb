13e8fbc538874fe0b3fc3ceab3663547b04fc9d7
Katrina <pritckat@gmail.com>
2018-10-04 14:19:49 -0500

    new user form

--- library-app/db/schema.rb
+++ library-app/db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_191219) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
