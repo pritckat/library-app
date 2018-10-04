e088a34f8297081e2be3d54d9e7fe85cfae46088
Katrina <pritckat@gmail.com>
2018-10-04 15:05:22 -0500

    user can log out

--- library-app/app/controllers/sessions_controller.rb
+++ library-app/app/controllers/sessions_controller.rb
@@ -14,4 +14,9 @@ class SessionsController < ApplicationController
             render login_path
         end
     end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to login_path
+    end
 end
