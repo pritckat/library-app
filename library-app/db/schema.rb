66d9353d04f95d0cd4bd2c23b4d02540c68d089f
Katrina <pritckat@gmail.com>
2018-10-04 16:42:50 -0500

    create author resource

--- library-app/db/schema.rb
+++ library-app/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_212703) do
+ActiveRecord::Schema.define(version: 2018_10_04_214223) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
