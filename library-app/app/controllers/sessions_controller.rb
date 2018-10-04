7b6e5ad08f4c4d5b66d52315f2ace666d40fcc13
Katrina <pritckat@gmail.com>
2018-10-04 16:00:37 -0500

    add alert for invalid log in credentials

--- library-app/app/controllers/sessions_controller.rb
+++ library-app/app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            render login_path
+            flash.alert = "Incorrect Credentials"
+            redirect_to login_path
         end
     end
 
