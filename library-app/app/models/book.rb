31d5f16350018ef9d39095658c1fbe58cf559521
Katrina <pritckat@gmail.com>
2018-10-04 16:51:00 -0500

    create join table for books and authors

--- library-app/app/models/book.rb
+++ library-app/app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
     has_many :library_books
     has_many :libraries, through: :library_books
+    has_many :book_authors
+    has_many :authors, through: :book_authors
 end
