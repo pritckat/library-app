75b83b86e5aa8d06bbcc0e2e2b129bfdaf2408b3
Katrina <pritckat@gmail.com>
2018-10-05 16:50:37 -0500

    add author names helper for displaying multiple authors

--- library-app/app/helpers/books_helper.rb
+++ library-app/app/helpers/books_helper.rb
@@ -1,3 +1,12 @@
 module BooksHelper
 
+    def author_names(book)
+        if book.authors.count == 1
+            book.authors.first.full_name
+        else 
+            names = book.authors.map { |a| a.full_name }
+            names.join(", ")
+        end
+    end
+
 end
