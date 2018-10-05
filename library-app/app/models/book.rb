76508014bbd761c476888c426d265a80739c990d
Katrina <pritckat@gmail.com>
2018-10-05 18:32:38 -0500

    book can only have one author

--- library-app/app/models/book.rb
+++ library-app/app/models/book.rb
@@ -1,16 +1,14 @@
 class Book < ApplicationRecord
     has_many :library_books
     has_many :libraries, through: :library_books
-    has_many :book_authors
-    has_many :authors, through: :book_authors
-    accepts_nested_attributes_for :authors
+    belongs_to :author
 
     def author_attributes=(author)
         names = author.split(" ")
         first_name = names.first
         last_name = names.last
-        self.authors << Author.find_or_create_by(first_name: first_name, last_name: last_name)
-        self.save
+        self.author = Author.find_or_create_by(first_name: first_name, last_name: last_name)
+        self.author.update(author)
     end
 
 end
