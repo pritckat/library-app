5a95dd68bd2d22d93c1e8698f819f604a9ccc6cb
Katrina <pritckat@gmail.com>
2018-10-05 16:06:17 -0500

    create association user has many books

--- library-app/app/models/user.rb
+++ library-app/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :libraries
+    has_many :books, through: :libraries
     validates :username, presence: :true
     validates :username, uniqueness: true
 end
