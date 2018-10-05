d44822fdaff3a89d3e149486908ad9673ab05627
Katrina <pritckat@gmail.com>
2018-10-04 21:23:11 -0500

    can create book from library page

--- library-app/app/controllers/books_controller.rb
+++ library-app/app/controllers/books_controller.rb
@@ -3,4 +3,19 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
     end
+    
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.create(book_params)
+        redirect_to book_path(@book)
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :language)
+    end
 end
