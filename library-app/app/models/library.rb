af2f00a204487ea6f778b724f92fd0ba9ce9ddd3
Katrina <pritckat@gmail.com>
2018-10-10 13:11:31 -0500

    add list of mandatory libraries to library model

--- library-app/app/models/library.rb
+++ library-app/app/models/library.rb
@@ -4,6 +4,12 @@ class Library < ApplicationRecord
     has_many :books, through: :library_books
     accepts_nested_attributes_for :books, reject_if: :all_blank
 
+    LIST = ["New", "On Loan", "Loaned"]
+
+    def self.mandatory
+        LIST
+    end
+
     def remove_empty_books
         self.books.each do |book|
             if book.title == ""
