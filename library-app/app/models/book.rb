96968b177064bf675104c26a4a92be5d509516ad
Katrina <pritckat@gmail.com>
2018-10-09 23:38:34 -0500

    name authors "None" if no author is given during book creation

--- library-app/app/models/book.rb
+++ library-app/app/models/book.rb
@@ -9,9 +9,12 @@ class Book < ApplicationRecord
         if names.count > 1
             first_name = names[0..-2].join(" ")
             last_name = names.last
+        elsif author.strip == ""
+            first_name = ""
+            last_name = "None"
         else
             first_name = ""
-            last_name = names
+            last_name = names.join
         end
         self.author = Author.find_or_create_by(first_name: first_name, last_name: last_name)
         self.save
